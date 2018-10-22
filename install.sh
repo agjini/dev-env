4edfae961a1c2eaa3c64fb6ea059906654310934
Baptiste Le Bail <blebail@protnomail.com>
2018-10-22 14:16:11 +0200

    (install) add prompt for sudo password

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook install.yml --extra-vars "lsb_release=$(lsb_release -cs)"
+ansible-playbook install.yml --extra-vars "lsb_release=$(lsb_release -cs)" --ask-become-pass
